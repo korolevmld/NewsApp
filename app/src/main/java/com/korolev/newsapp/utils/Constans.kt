4d5a4a587ca2ec5d77a6f15e6a6c811b709f35c7
korolevmld <zhkorolev@mail.ru>
2022-06-01 21:03:43 +0300

    Lesson-3

---
+++ app/src/main/java/com/korolev/newsapp/utils/Constans.kt
@@ -0,0 +1,9 @@
+package com.korolev.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "bdf3c4b960094bdfa8c4e041354a96ed"
+
+    }
+}
